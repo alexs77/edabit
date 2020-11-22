82a32522e5e65a652536140146edb06700803397
Alexander Skwar <alexander@tutti.ch>
2020-11-22 09:52:29 +0100

    Add (slightly obfuscated) solution.

---
+++ Perimeter/perimeter_obf.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def k8 (O00O000O00O00OOO0 ,O0000000OOOOO0OOO ):#line:3
+    OOOO0OOO0O00OO000 =(O00O000O00O00OOO0 +O0000000OOOOO0OOO )*2 #line:8
+    return OOOO0OOO0O00OO000 #line:9
+assert k8 (6 ,7 )==26 #line:11
+assert k8 (20 ,10 )==60 #line:12
+assert k8 (2 ,9 )==22 #line:13
+print ("find_perimeter() works correctly")#line:15
